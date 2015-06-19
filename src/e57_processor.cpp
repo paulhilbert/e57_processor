e95c9e72c6274c3558f9f4503da33c134d2763b8
Paul Hilbert <me@sicgraph.org>
2015-06-19 10:57:01 +0200

    BROKENVERSION: Unusual docker test commit

--- src/e57_processor.cpp
+++ src/e57_processor.cpp
@@ -54,6 +54,10 @@ int main (int argc, char const* argv[]) {
 		("kNN,k",        po::value<uint32_t>(&kNN)->default_value(12), "Number of nearest neighbors to use for normal estimation (default is 0; value of 0 means no normal estimation)")
 	;
 
+    std::cout << "leaf: " << leafSize << "\n";
+    std::cout << "kNN: " << kNN << "\n";
+    return 0;
+
 	// Check for required options.
 	po::variables_map vm;
 	bool optionsException = false;
