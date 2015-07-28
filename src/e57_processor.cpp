6a7b83fdbc4f6954c732539bbb299adc46c268e9
Ochi <ochi@arcor.de>
2015-07-28 12:06:18 +0200

    Processing in local coordinate systems

--- src/e57_processor.cpp
+++ src/e57_processor.cpp
@@ -33,47 +33,46 @@ typedef pcl::PointCloud<PointXYZ> CloudXYZ;
 typedef pcl::PointCloud<PointNormal> CloudNormal;
 
 std::pair<CloudXYZ::Ptr, CloudNormal::Ptr> process_cloud(CloudXYZ::Ptr cloud, optional<float> leaf_size, optional<uint32_t> kNN);
-Eigen::Vector3d read_origin(e57::Reader& reader, uint32_t scan_index);
-CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, const Eigen::Vector3d& offset);
-void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr normal_cloud);
+CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, e57::RigidBodyTransform& scanTransform);
+void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr normal_cloud, const e57::RigidBodyTransform& scanTransform);
 
 typedef std::vector<double> coords_t;
 typedef std::tuple<coords_t, coords_t, coords_t> point_data_t;
 
 
 int main (int argc, char const* argv[]) {
-	std::string  fileIn;
-	std::string  fileOut;
-	std::string  prefixOut;
+    std::string  fileIn;
+    std::string  fileOut;
+    std::string  prefixOut;
     float        leafSize;
     uint32_t     kNN;
 
-	po::options_description desc("Graphene command line options");
-	desc.add_options()
-		("help,h",  "Help message")
-		("input,i",     po::value<std::string>(&fileIn) ->required(), "Input E57 File")
-		("output,o",     po::value<std::string>(&fileOut), "Output E57 File (optionally use --prefix instead)")
-		("prefix,p",  po::value<std::string>(&prefixOut), "Prefix to use for output files")
-		("leaf,l",   po::value<float>(&leafSize)->default_value(0.0), "Leaf Size for subsampling (default is 0; size <= 0 means no subsampling)")
-		("kNN,k",        po::value<uint32_t>(&kNN)->default_value(12), "Number of nearest neighbors to use for normal estimation (default is 0; value of 0 means no normal estimation)")
-	;
+    po::options_description desc("E57 processor command line options");
+    desc.add_options()
+        ("help,h",  "Help message")
+        ("input,i",     po::value<std::string>(&fileIn) ->required(), "Input E57 File")
+        ("output,o",     po::value<std::string>(&fileOut), "Output E57 File (optionally use --prefix instead)")
+        ("prefix,p",  po::value<std::string>(&prefixOut), "Prefix to use for output files")
+        ("leaf,l",   po::value<float>(&leafSize)->default_value(0.0), "Leaf Size for subsampling (default is 0; size <= 0 means no subsampling)")
+        ("kNN,k",        po::value<uint32_t>(&kNN)->default_value(12), "Number of nearest neighbors to use for normal estimation (default is 0; value of 0 means no normal estimation)")
+    ;
 
-	// Check for required options.
-	po::variables_map vm;
-	bool optionsException = false;
-	try {
-		po::store(po::parse_command_line(argc, argv, desc), vm);
-		po::notify(vm);
-	} catch (std::exception& e) {
-		if (!vm.count("help")) {
-			std::cout << e.what() << "\n";
-		}
-		optionsException = true;
-	}
-	if (optionsException || vm.count("help")) {
-		std::cout << desc << "\n";
-		return optionsException ? 1 : 0;
-	}
+    // Check for required options.
+    po::variables_map vm;
+    bool optionsException = false;
+    try {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    } catch (std::exception& e) {
+        if (!vm.count("help")) {
+            std::cout << e.what() << "\n";
+        }
+        optionsException = true;
+    }
+    if (optionsException || vm.count("help")) {
+        std::cout << desc << "\n";
+        return optionsException ? 1 : 0;
+    }
 
     fs::path pFileIn(fileIn);
     if (!fs::exists(pFileIn)) {
@@ -99,59 +98,50 @@ int main (int argc, char const* argv[]) {
         suffixOut = pFileOut.extension().string();
     }
 
-	try {
-		e57::Reader reader(fileIn);
+    try {
+        e57::Reader reader(fileIn);
         e57::E57Root root;
         reader.GetE57Root(root);
         std::string output_file = prefixOut + suffixOut;
         std::cout << "output file: " << output_file << "\n";
-		e57::Writer writer_xyz(output_file, root.coordinateMetadata);
+        e57::Writer writer_xyz(output_file, root.coordinateMetadata);
 
-		uint32_t scanCount = reader.GetData3DCount();
-		uint32_t imgCount = reader.GetImage2DCount();
-		std::cout << "number of scans: " << scanCount << "\n";
-		std::cout << "number of images: " << imgCount << "\n\n";
+        uint32_t scanCount = reader.GetData3DCount();
+        uint32_t imgCount = reader.GetImage2DCount();
+        std::cout << "number of scans: " << scanCount << "\n";
+        std::cout << "number of images: " << imgCount << "\n\n";
 
-        Eigen::Vector3d first_origin = read_origin(reader, 0);
-
-		for (uint32_t scan_index = 0; scan_index < scanCount; ++scan_index) {
-			std::cout << "scan: " << (scan_index+1) << " / " << scanCount << "\n";
-            CloudXYZ::Ptr cloud = read_scan(reader, scan_index, -first_origin);
+        for (uint32_t scan_index = 0; scan_index < scanCount; ++scan_index) {
+            std::cout << "scan: " << (scan_index+1) << " / " << scanCount << "\n";
+            e57::RigidBodyTransform scanTransform;
+            CloudXYZ::Ptr cloud = read_scan(reader, scan_index, scanTransform);
 
             optional<float> ls = boost::make_optional(leafSize > 0.f, leafSize);
             optional<uint32_t> k = boost::make_optional(kNN > 0, kNN);
             CloudNormal::Ptr normal_cloud;
             std::tie(cloud, normal_cloud) = process_cloud(cloud, ls, k);
 
-            write_scan(writer_xyz, cloud, normal_cloud);
-		}
+            write_scan(writer_xyz, cloud, normal_cloud, scanTransform);
+        }
 
         writer_xyz.Close();
-	} catch (e57::E57Exception& e) {
+    } catch (e57::E57Exception& e) {
         std::cout << "Exception thrown:" << "\n";
-		std::cout << e.what() << "\n";
+        std::cout << e.what() << "\n";
         std::cout << "Context:" << "\n";
         std::cout << e.context() << "\n";
-	}
+    }
 
-	return 0;
+    return 0;
 }
 
 CloudNormal::Ptr compute_normals(CloudXYZ::Ptr cloud, uint32_t k) {
-    for (auto& p : *cloud) {
-        p.getVector3fMap() -= cloud->sensor_origin_.head(3);
-    }
-
     CloudNormal::Ptr normal_cloud(new CloudNormal());
     pcl::NormalEstimation<PointXYZ, PointNormal> ne;
     ne.setInputCloud(cloud);
     ne.setKSearch(k);
     ne.setViewPoint(0.f, 0.f, 0.f);
     ne.compute(*normal_cloud);
-
-    for (auto& p : *cloud) {
-        p.getVector3fMap() += cloud->sensor_origin_.head(3);
-    }
 
     return normal_cloud;
 }
@@ -180,13 +170,7 @@ std::pair<CloudXYZ::Ptr, CloudNormal::Ptr> process_cloud(CloudXYZ::Ptr cloud, op
     return {cloud, normal_cloud};
 }
 
-Eigen::Vector3d read_origin(e57::Reader& reader, uint32_t scan_index) {
-    e57::Data3D header;
-    reader.ReadData3D(scan_index, header);
-    return Eigen::Vector3d(header.pose.translation.x, header.pose.translation.y, header.pose.translation.z);
-}
-
-CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, const Eigen::Vector3d& offset) {
+CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, e57::RigidBodyTransform& scanTransform) {
     e57::Data3D header;
     reader.ReadData3D(scan_index, header);
     int64_t nColumn = 0, nRow = 0, nPointsSize = 0, nGroupsSize = 0, nCounts = 0; bool bColumnIndex = 0;
@@ -194,25 +178,20 @@ CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, const Eigen::V
 
     int64_t n_size = (nRow > 0) ? nRow : 1024;
 
-    double *data_x = new double[n_size], *data_y = new double[n_size], *data_z = new double[n_size], *intensity = new double[n_size];
-    auto block_read = reader.SetUpData3DPointsData(scan_index, n_size, data_x, data_y, data_z, NULL, intensity);
+    double *data_x = new double[n_size], *data_y = new double[n_size], *data_z = new double[n_size];
+    auto block_read = reader.SetUpData3DPointsData(scan_index, n_size, data_x, data_y, data_z);
 
     unsigned long size = 0;
-    float imin = header.intensityLimits.intensityMinimum;
-    float imax = header.intensityLimits.intensityMaximum;
-    std::vector<Eigen::Vector3d> points;
-    std::vector<float> intensities;
-    uint32_t idx = 0;
+    CloudXYZ::Ptr cloud(new CloudXYZ());
     while((size = block_read.read()) > 0) {
         for(unsigned long i = 0; i < size; i++) {
-            Eigen::Vector3d p;
-            p[0] = data_x[i];
-            p[1] = data_y[i];
-            p[2] = data_z[i];
-            points.push_back(p);
-            intensities.push_back((intensity[i]-imin) / (imax-imin));
-            //p.intensity = (intensity[i]-imin) / (imax-imin);
-            //cloud->push_back(p);
+            PointXYZ p_pcl;
+            p_pcl.getVector3fMap() = Eigen::Vector3f(
+                static_cast<float>(data_x[i]),
+                static_cast<float>(data_y[i]),
+                static_cast<float>(data_z[i])
+            );
+            cloud->push_back(p_pcl);
         }
     }
     block_read.close();
@@ -221,26 +200,8 @@ CloudXYZ::Ptr read_scan(e57::Reader& reader, uint32_t scan_index, const Eigen::V
     delete [] data_y;
     delete [] data_z;
 
-    // transform by registration transform
-    Eigen::Affine3d registration;
-    registration = Eigen::Quaterniond(header.pose.rotation.w, header.pose.rotation.x, header.pose.rotation.y, header.pose.rotation.z);
-    registration = Eigen::Translation<double, 3>(header.pose.translation.x + offset[0], header.pose.translation.y + offset[1], header.pose.translation.z + offset[2]) * registration;
-
-    // copy to pointcloud
-    CloudXYZ::Ptr cloud(new CloudXYZ());
-    for (auto& p : points) {
-        p = registration * p;
-        PointXYZ p_pcl;
-        p_pcl.getVector3fMap() = p.template cast<float>();
-        p_pcl.intensity = intensities[idx++];
-        cloud->push_back(p_pcl);
-    }
-
-
-    //Eigen::Vector4f translation(header.pose.translation.x + offset[0], header.pose.translation.y + offset[1], header.pose.translation.z + offset[2], 0.f);
-    //cloud->sensor_origin_ = translation;
-    //Eigen::Quaternionf rotation(header.pose.rotation.w, header.pose.rotation.x, header.pose.rotation.y, header.pose.rotation.z);
-    //pcl::transformPointCloud(*cloud, *cloud, translation.head(3), rotation);
+    // Remember the original scan pose for later writing.
+    scanTransform = header.pose;
 
     return cloud;
 }
@@ -255,12 +216,10 @@ void write_scan_data(e57::Writer& writer, e57::Data3D& header, point_data_t& pos
     header.pointFields.cartesianXField = true;
     header.pointFields.cartesianYField = true;
     header.pointFields.cartesianZField = true;
-    //header.pointFields.cartesianInvalidStateField = true;
     if (has_normals) {
         header.pointFields.sphericalRangeField = true;
         header.pointFields.sphericalAzimuthField = true;
         header.pointFields.sphericalElevationField = true;
-        //header.pointFields.sphericalInvalidStateField = true;
     }
 
     int scan_index = writer.NewData3D(header);
@@ -283,7 +242,7 @@ void write_scan_data(e57::Writer& writer, e57::Data3D& header, point_data_t& pos
     block_write.close();
 }
 
-void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr normal_cloud) {
+void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr normal_cloud, const e57::RigidBodyTransform& scanTransform) {
     // write positions
     e57::Data3D header;
     boost::uuids::random_generator gen;
@@ -298,9 +257,9 @@ void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr norma
     header.pointFields.cartesianXField = true;
     header.pointFields.cartesianYField = true;
     header.pointFields.cartesianZField = true;
-    header.pose.translation.x = cloud->sensor_origin_[0];
-    header.pose.translation.y = cloud->sensor_origin_[1];
-    header.pose.translation.z = cloud->sensor_origin_[2];
+    
+    // Copy the original scan pose.
+    header.pose = scanTransform;
 
     // write position data
     bool has_normals = normal_cloud && normal_cloud->size() == cloud->size();
@@ -319,18 +278,4 @@ void write_scan(e57::Writer& writer, CloudXYZ::Ptr cloud, CloudNormal::Ptr norma
         ++idx;
     }
     write_scan_data(writer, header, pos_data, nrm_data);
-
-    // write normal data
-    //if (!normal_cloud) return;
-    //header.pose.translation.x = 0.0;
-    //header.pose.translation.y = 0.0;
-    //header.pose.translation.z = 0.0;
-    //idx = 0;
-    //for (const auto& p : *normal_cloud) {
-        //data_x[idx] = p.normal[0];
-        //data_y[idx] = p.normal[1];
-        //data_z[idx] = p.normal[2];
-        //++idx;
-    //}
-    //write_scan_data(writer_normals, header, data_x, data_y, data_z);
 }
