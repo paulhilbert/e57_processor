389b4922df12b64e2d5201590ca125c54da6eeb1
Paul Hilbert <me@sicgraph.org>
2015-12-07 12:58:53 +0100

    missing docker_build

--- docker_build.sh
+++ docker_build.sh
@@ -4,9 +4,9 @@ PROJECT="$1"
 
 cd /tmp
 git clone https://github.com/paulhilbert/${PROJECT}.git
-git checkout explicit_break
 mkdir -p ${PROJECT}/build
 cd ${PROJECT}/build
+git checkout explicit_break
 cmake -D CMAKE_INSTALL_PREFIX="/usr" -D CMAKE_BUILD_TYPE="Release" ..
 make
 make install
